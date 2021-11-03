1d973b66f4a98f10b386aa7cd60f700ed806f027
Gavin Corkery <GavinCorkery@hotmail.com>
2021-11-03 15:56:01 -0400

    Changed navbar styling

--- transaction-manager/src/components/NavBar/Navbar.jsx
+++ transaction-manager/src/components/NavBar/Navbar.jsx
@@ -5,20 +5,19 @@ import './Navbar.css'
 const Navbar = (props) => {
     return (
         <div className="row navbar">
-            <div className="col-lg-3" align = "right">
-                <nav>
-                    <ul>
-                        <Link to = '/' className = "nav-item">
-                            <li>Home</li>
-                        </Link>
-                        <Link to = '/login' className = "nav-item">
-                            <li>Log In</li>
-                        </Link>
-                        <Link to = '/register' className = "nav-item">
-                            <li>Register</li>
-                        </Link>
-                    </ul>
-                </nav>
+            <div className="col-lg-8"></div>
+            <div className="col-lg-4" align = "center">
+                <ul>
+                    <Link to = '/' className = "nav-item">
+                        <li>Home</li>
+                    </Link>
+                    <Link to = '/login' className = "nav-item">
+                        <li>Log In</li>
+                    </Link>
+                    <Link to = '/register' className = "nav-item">
+                        <li>Register</li>
+                    </Link>
+                </ul>
             </div>
         </div>
      );
