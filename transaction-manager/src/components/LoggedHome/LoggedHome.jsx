116109b0bef5ba12d0b00ac951bdfaaae9121e66
Gavin Corkery <GavinCorkery@hotmail.com>
2021-11-04 16:42:49 -0400

    RecentTransactions component made. Needs to be styled

--- transaction-manager/src/components/LoggedHome/LoggedHome.jsx
+++ transaction-manager/src/components/LoggedHome/LoggedHome.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import jwtDecode from 'jwt-decode'
+import RecentTransactions from '../RecentTransactions/RecentTransactions'
 
 
 
@@ -9,13 +10,11 @@ const LoggedHome = (props) => {
     const [userLedgers, setUserLedgers] = useState(null)
     const [user, setUser] = useState(null)
     const [dataReady, setReady] = useState(false)
-    const [transactions, setTransactions] = useState(null)
 
     useEffect(() => {
         if (localStorage.getItem('token')){
           getUserInfo()
           getUserLedgers()
-          getUserTransactions()
         }
       }, [])
 
@@ -29,22 +28,12 @@ const LoggedHome = (props) => {
       }
     }
 
-    const getUserTransactions = async() => {
-        const jwt = localStorage.getItem('token')
-        try{
-            let transactions = await axios.get('http://127.0.0.1:8000/api/transactions/', {headers: {'Authorization': 'Bearer ' + jwt}})
-            setTransactions(transactions.data)
-            setReady(true)
-        }catch (err){
-            console.log("🚀 ~ file: LoggedHome.jsx ~ line 39 ~ getUserTransactions ~ err", err)
-        }
-    }
-
     const getUserLedgers = async() => {
         try{
             const jwt = localStorage.getItem('token')
             let response = await axios.get('http://127.0.0.1:8000/api/ledgers/', {headers: {'Authorization': 'Bearer ' + jwt}})
             setUserLedgers(response.data)
+            setReady(true)
         }catch(err) {
             console.log("🚀 ~ file: LoggedHome.jsx ~ line 13 ~ getUserLedgers ~ err", err)
         }
@@ -60,9 +49,7 @@ const LoggedHome = (props) => {
                     return <p>{ledger.name} {ledger.total}</p>
                 })}
                 <p>User Transactions:</p>
-                {transactions.map((transaction) => {
-                    return <p>{transaction.date} {transaction.place} {transaction.category} {transaction.total}</p>
-                })}
+                <RecentTransactions />
             </div>
         </div>)
         :
---
+++ transaction-manager/src/components/RecentTransactions/RecentTransactions.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {Table} from 'react-bootstrap'
+
+const RecentTransactions = (props) => {
+
+    const [transactions, setTransactions] = useState(null)
+    const [dataReady, setReady] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('token')){
+          getUserTransactions()
+        }
+      }, [])
+
+    const getUserTransactions = async() => {
+        const jwt = localStorage.getItem('token')
+        try{
+            let transactions = await axios.get('http://127.0.0.1:8000/api/transactions/', {headers: {'Authorization': 'Bearer ' + jwt}})
+            setTransactions(transactions.data)
+            setReady(true)
+        }catch (err){
+            console.log("🚀 ~ file: LoggedHome.jsx ~ line 39 ~ getUserTransactions ~ err", err)
+        }
+    }
+
+    return (
+        dataReady ? 
+        (<Table striped border hover>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Place</th>
+                    <th>Category</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                {transactions.map((transaction) => {
+                    return  <tr>
+                            <td>{transaction.date}</td>
+                            <td>{transaction.place}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.total}</td>
+                            </tr>
+                })}
+            </tbody>
+        </Table>)
+        : null
+     );
+}
+ 
+export default RecentTransactions;
